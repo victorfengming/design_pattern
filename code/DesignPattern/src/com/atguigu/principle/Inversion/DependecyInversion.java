f66933e7dafe6155eadc0a18bff5f4d721c867b4
于凤明 <yufengming@travelsky.com>
2021-02-01 16:39:41 +0800

    方式2

--- code/DesignPattern/src/com/atguigu/principle/Inversion/DependecyInversion.java
+++ code/DesignPattern/src/com/atguigu/principle/Inversion/DependecyInversion.java
@@ -17,6 +17,7 @@ package com.atguigu.principle.Inversion;
 public class DependecyInversion {
 
     public static void main(String[] args) {
+        // 客户端无需改变
         Person p = new Person();
         p.receive(new Email());
 
@@ -24,27 +25,25 @@ public class DependecyInversion {
 
 }
 
-
-class Email{
+// 定义接口
+interface IReceiver{
+    public String getInfo();
+}
+class Email implements IReceiver{
     public String getInfo() {
         return " 电子邮件信息:hellworld";
     }
 
 }
+
+
 /*
 完成 Person 接收消息功能
-* 方式1 : 分析
-1. 简单呐, 比较容易能够想到
-2. 如果,我们获取的对象是 微信 ,短信 等等,则 要新增类
-3. 解决思路: 引入一个抽象的接口IReceiver, 表示接收者, 这样Person
-
-类与接口 IReceiver 发生依赖
-
-因为Email,WeiXin 等等 属于接收范围 ,他们各自实现IReceiver 接口就 OK,
+* 方式2
 
 * */
 class Person{
-    public void receive(Email email) {
-        System.out.println(email.getInfo());
+    public void receive(IReceiver iReceiver) {
+        System.out.println(iReceiver.getInfo());
     }
 }
