ebe5b75260b59afdd8e1565b799c7052048d9bf6
上官一明 <victorfengming@163.com>
2021-02-02 20:34:13 +0800

    ,

--- code/DesignPattern/src/com/atguigu/principle/singleton/type06/SingletonTest06.java
+++ code/DesignPattern/src/com/atguigu/principle/singleton/type06/SingletonTest06.java
@@ -39,7 +39,7 @@ class Singleton {
     }
 
     // 提供一个静态的公有方法,加入双重检查代码
-    // ,解决线程安全问题,同时解决懒加载问题
+    // 解决线程安全问题,同时解决懒加载问题
     public static Singleton getInstance() {
         if (instance == null) {
             synchronized (Singleton.class) {
