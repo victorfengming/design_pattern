7708c14a13ccd44884699169d2f1e95fc6d387a8
上官一明 <victorfengming@163.com>
2021-02-02 18:34:09 +0800

    开放 封闭+

---
+++ code/DesignPattern/src/com/atguigu/principle/ocp/Ocp.java
@@ -0,0 +1,14 @@
+package com.atguigu.principle.ocp;
+
+/**
+ * @author victor
+ * @site https://victorfengming.github.io/
+ * @comp234 方ny XDL
+ * @project java_mode
+ * @package com.atguigu.principle.ocp
+ * @created 2021-02-01 20:19
+ * @function ""
+ */
+public class Ocp {
+
+}
