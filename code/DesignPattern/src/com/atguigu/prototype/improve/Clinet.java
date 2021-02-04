cd3c03fc2b7521c3c52bd9bccff915a9a7e41687
于凤明 <yufengming@travelsky.com>
2021-02-04 13:21:50 +0800

    使用原型模式连解决了克隆羊的问题

---
+++ code/DesignPattern/src/com/atguigu/prototype/improve/Clinet.java
@@ -0,0 +1,26 @@
+package com.atguigu.prototype.improve;
+
+public class Clinet {
+    public static void main(String[] args) {
+        // 原型模式完成 对象的创建
+        Sheep sheep = new Sheep("tom", 1, "白色");
+        Sheep sheep2 = (Sheep) sheep.clone();   // 克隆
+        Sheep sheep3 = (Sheep) sheep.clone();   // 克隆
+        Sheep sheep4 = (Sheep) sheep.clone();   // 克隆
+
+        System.out.println(sheep);
+        System.out.println(sheep2);
+        System.out.println(sheep3);
+        System.out.println(sheep4);
+
+        /*
+        * Sheep{name='tom', age=1, color='白色'}
+        Sheep{name='tom', age=1, color='白色'}
+        Sheep{name='tom', age=1, color='白色'}
+        Sheep{name='tom', age=1, color='白色'}
+
+        Process finished with exit code 0
+
+        * */
+    }
+}
