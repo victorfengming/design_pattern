631a9d938bfb6d79d14b22a9492e7b7591ab9daf
于凤明 <yufengming@travelsky.com>
2021-02-04 15:25:53 +0800

    方式2 完成的深拷贝

--- code/DesignPattern/src/com/atguigu/prototype/deepclone/Clinet.java
+++ code/DesignPattern/src/com/atguigu/prototype/deepclone/Clinet.java
@@ -7,8 +7,8 @@ public class Clinet {
         p.name = "松江";
         p.deepCloneableTarget = new DeepCloneableTarget("大牛", "小牛");
 
-        //方式1 完成深拷贝
-        DeepProtoType p2 = (DeepProtoType) p.clone();
+        // 方式2 完成深拷贝
+        DeepProtoType p2 = (DeepProtoType) p.deepClone();
 
         System.out.println(p);
         System.out.println(p.name);
@@ -17,19 +17,15 @@ public class Clinet {
         System.out.println(p2.name);
         System.out.println(p2.deepCloneableTarget);
         /*
-        com.atguigu.prototype.deepclone.DeepProtoType@1540e19d
+        com.atguigu.prototype.deepclone.DeepProtoType@330bedb4
         松江
-        com.atguigu.prototype.deepclone.DeepCloneableTarget@677327b6
-        com.atguigu.prototype.deepclone.DeepProtoType@14ae5a5
+        com.atguigu.prototype.deepclone.DeepCloneableTarget@2503dbd3
+        com.atguigu.prototype.deepclone.DeepProtoType@4f3f5b24
         松江
-        com.atguigu.prototype.deepclone.DeepCloneableTarget@7f31245a
+        com.atguigu.prototype.deepclone.DeepCloneableTarget@15aeb7ab
 
         Process finished with exit code 0
+        * */
 
-        * */
-        /*
-        * todo 这种方式 就是可以 实现深拷贝
-        * 说白了就是,在里面 提取出来属性对象, 单独进行拷贝
-        * */
     }
 }
--- code/DesignPattern/src/com/atguigu/prototype/deepclone/DeepProtoType.java
+++ code/DesignPattern/src/com/atguigu/prototype/deepclone/DeepProtoType.java
@@ -1,6 +1,6 @@
 package com.atguigu.prototype.deepclone;
 
-import java.io.Serializable;
+import java.io.*;
 
 
 public class DeepProtoType implements Serializable,Cloneable {
@@ -27,4 +27,53 @@ public class DeepProtoType implements Serializable,Cloneable {
     /*
     * 问题: 这样不会递归拷贝么?
     * */
+
+
+    // 深拷贝 -- 方式2 通过对象的序列化实现(推荐)
+    public Object deepClone(){
+
+        // 创建流对象
+        ByteArrayOutputStream bos = null;
+        ObjectOutputStream oos = null;
+        ByteArrayInputStream bis = null;
+        ObjectInputStream ois = null;
+
+        try {
+            // 序列化
+            bos = new ByteArrayOutputStream();
+            // 吧这个字节数组输出流换成了对象输出流
+            oos = new ObjectOutputStream(bos);
+            // 把当前这个对象以对象流的方式输出
+            oos.writeObject(this);
+
+            // 反序列化
+            bis = new ByteArrayInputStream(bos.toByteArray());
+            // 读进来就相当于把我们这个对象进行了克隆
+            ois = new ObjectInputStream(bis);
+            // ois就读到了原来序列化出去的这个对象
+            DeepProtoType copyObj = (DeepProtoType)ois.readObject();
+            // 我用序列化这个特点,把你关联的引用的输出出去,读回来后就相当云拷贝了
+            return copyObj;
+            // 我们这波操作就是充分利用我们的序列化和饭序列化,
+            // 把当前对象以对象流的方式输出和读回来,就不用一个一个来搞了
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭已经打开的流
+            try {
+                bos.close();
+                oos.close();
+                bis.close();
+                ois.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println(e.getMessage());
+            }
+        }
+
+        return null;
+    }
 }
