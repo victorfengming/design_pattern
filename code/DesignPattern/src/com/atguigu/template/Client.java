16562db473c6c4f6378b5b95e007007bddff3c19
于凤明 <yufengming@travelsky.com>
2021-02-08 12:32:21 +0800

    代码实践

---
+++ code/DesignPattern/src/com/atguigu/template/Client.java
@@ -0,0 +1,32 @@
+package com.atguigu.template;
+
+
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("---------");
+        // 制作红豆豆浆
+        System.out.println("----制作红豆豆浆----");
+        SoyaMilk redBeanSoyaMilk = new RedBeanSoyaMilk();
+        redBeanSoyaMilk.make();
+
+        System.out.println("----制作花生豆浆----");
+        SoyaMilk peanutSoyMilk = new PeanutSoyaMilk();
+        peanutSoyMilk.make();
+
+        /**
+         * ---------
+         * ----制作红豆豆浆----
+         * 第一步: 选择好的新鲜黄豆
+         * 加入上号的红豆
+         * 第三部,黄豆和配料开始浸泡,需要3小时
+         * 第四部,黄豆和配料放到豆浆机去打碎!
+         * ----制作花生豆浆----
+         * 第一步: 选择好的新鲜黄豆
+         * 加入上号的花生!
+         * 第三部,黄豆和配料开始浸泡,需要3小时
+         * 第四部,黄豆和配料放到豆浆机去打碎!
+         *
+         * Process finished with exit code 0
+         */
+    }
+}
---
+++ code/DesignPattern/src/com/atguigu/template/RedBeanSoyaMilk.java
@@ -0,0 +1,9 @@
+package com.atguigu.template;
+
+
+public class RedBeanSoyaMilk extends SoyaMilk{
+    @Override
+    void addCondiments() {
+        System.out.println("加入上号的红豆");
+    }
+}
