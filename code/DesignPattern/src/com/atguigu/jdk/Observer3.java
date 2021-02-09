51ff4ad86506156367c92f0e3c160e57f1ec88d8
于凤明 <yufengming@travelsky.com>
2021-02-09 11:05:51 +0800

    观察者模式 over

---
+++ code/DesignPattern/src/com/atguigu/jdk/Observer3.java
@@ -0,0 +1,21 @@
+package com.atguigu.jdk;
+
+/**
+ * ClassName:  <br/>
+ * Description:  <br/>
+ * Date: 2021-02-09 10:48 <br/>
+ * <br/>
+ *
+ * @author yufengming
+ * @version 产品版本信息 2021年02月09日10:48分 yufengming(yufengming@travelsky.com) 新建<br/>
+ * <p>
+ * 修改记录
+ * @email yufengming@travelsky.com
+ * @project java_mode
+ * @package com.atguigu.jdk
+ */
+public class Observer3 {
+    public static void main(String[] args) {
+        //Observable
+    }
+}
