421d22f6bf45b4b795ce51b407f017a409732df6
于凤明 <yufengming@travelsky.com>
2021-02-09 11:27:58 +0800

    终结者模式 案例

---
+++ code/DesignPattern/src/com/atguigu/mediator/Client.java
@@ -0,0 +1,14 @@
+package com.atguigu.mediator;
+
+/**
+ * ClassName:  <br/>
+ * Description:  <br/>
+ * Date: 2021-02-09 11:14 <br/>
+ * @project java_mode
+ * @package com.atguigu.mediator
+ */
+public class Client {
+    public static void main(String[] args) {
+
+    }
+}
